4c85563e1185e2aefc0bd9abd1a9137b71194c98
Yonas yalew <yonasyalewwerke@gmail.com>
2023-11-10 02:14:29 -0800

    task 0

---
+++ Test2/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int print_char(va_list val);
+int _printf(const char *format, ...);
+int print_s(va_list val);
+int _strlen(char *s);
+int _strlenc(const char *s);
+int print_37(void);
+
+#endif
---
+++ Test2/print_c.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printf_char - prints character
+ * @val: argument
+ * Return: 1.
+ */
+
+int print_char(va_list val)
+{
+	char str;
+
+	str = va_arg(val, int);
+	_putchar(str);
+	return (1);
+}

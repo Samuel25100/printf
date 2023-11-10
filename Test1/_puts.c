cf72d06a859502dc9b93eb4955f3c9e1d5c328f8
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-10 11:40:01 +0300

    add functions use for _printf.c

---
+++ Test1/_puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - print string of char
+ * @s: pointer to the string
+ * Return: number byte it printed
+ */
+int _puts(char *s)
+{
+	int len = _strlen(s);
+	return write(1, s, x);
+}
